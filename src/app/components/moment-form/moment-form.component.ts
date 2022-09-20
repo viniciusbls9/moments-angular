1ffe4ad505e6167b1d783f2ebcec9a1e38678bda
Vinicius <beneditoloura@gmail.com>
2022-09-20 07:45:25 -0300

    ✨ feat: create forms validations

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-moment-form',
@@ -8,7 +9,37 @@ import { Component, Input, OnInit } from '@angular/core';
 export class MomentFormComponent implements OnInit {
   @Input() btnText!: string;
 
+  momentForm!: FormGroup;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl(''),
+    });
+  }
+
+  get title() {
+    return this.momentForm.get('title')!;
+  }
+
+  get description() {
+    return this.momentForm.get('description')!;
+  }
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+
+    this.momentForm.patchValue({ image: file });
+  }
+
+  submit() {
+    if (this.momentForm.invalid) {
+      return;
+    }
+    console.log(this.momentForm.value);
+  }
 }
