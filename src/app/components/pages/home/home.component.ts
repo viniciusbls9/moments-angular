c525113c9af29e5a85ccf818a886f69ee9eca5f3
Vinicius <beneditoloura@gmail.com>
2022-09-21 07:07:38 -0300

    ✨ feat: get data moment

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MomentService } from 'src/app/services/moment.service';
+import { Moment } from 'src/app/Moment';
+import { environment } from 'src/environments/environment';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  allMoments: Moment[] = [];
+  moments: Moment[] = [];
+  baseApiUrl = environment.baseApiUrl;
 
-  constructor() { }
+  // todo search
+
+  constructor(private momentService: MomentService) {}
 
   ngOnInit(): void {
-  }
+    this.momentService.getMoments().subscribe((items) => {
+      const data = items.data;
 
+      data.map((item) => {
+        item.created_at = new Date(item.created_at!).toLocaleString('pt-BR');
+      });
+
+      this.allMoments = data;
+      this.moments = data;
+    });
+  }
 }
--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Moment } from '../Moment';
 
+import { Response } from '../Response';
+import { Moment } from '../Moment';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -13,6 +14,10 @@ export class MomentService {
   private apiUrl = `${this.baseApiUrl}api/moments`;
 
   constructor(private http: HttpClient) {}
+
+  getMoments(): Observable<Response<Moment[]>> {
+    return this.http.get<Response<Moment[]>>(this.apiUrl);
+  }
 
   createMoment(formData: FormData): Observable<FormData> {
     return this.http.post<FormData>(this.apiUrl, formData);
