4ba1ebba86753411f22f50059e4112cdcf8917ac
Vinicius <beneditoloura@gmail.com>
2022-09-21 07:33:26 -0300

    ✨ feat: create single moment page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { NewMomentComponent } from './components/pages/new-moment/new-moment.com
 import { MomentFormComponent } from './components/moment-form/moment-form.component';
 import { MessageComponent } from './components/message/message.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MomentComponent } from './components/page/moment/moment.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     NewMomentComponent,
     MomentFormComponent,
     MessageComponent,
+    MomentComponent,
   ],
   imports: [
     BrowserModule,
